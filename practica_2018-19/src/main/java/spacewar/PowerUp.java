7038d0927459d172cb9e3ac89a00b4412e6c18c8
Laura Suonperä Lozano <43136220+LauraLuna96@users.noreply.github.com>
2019-05-26 20:56:18 +0200

    ME ENCANTA TRABAJAR A LO TONTO - modificaciones en PowerUp
    resulta que mola más si hereda de otra clase

--- practica_2018-19/src/main/java/spacewar/PowerUp.java
+++ practica_2018-19/src/main/java/spacewar/PowerUp.java
@@ -2,28 +2,19 @@ package spacewar;
 
 // CLASE PADRE DE POWER UPS //
 
-public class PowerUp {
-	private final int posX;
-	private final int posY;
+public class PowerUp extends SpaceObject {
+
 	private final int id;
 	private static final int POWERUP_COLLISION_FACTOR = 200;
 
-	PowerUp(int posX, int posY, int id) {
-		this.posX = posX;
-		this.posY = posY;
+	public PowerUp(int id) {
 		this.id = id;
-	}
-
-	public int getPosX() {
-		return this.posX;
-	}
-
-	public int getPosY() {
-		return this.posY;
 	}
 
 	public int getId() {
 		return this.id;
 	}
+	
+	public void applyPowerUp(Player player) {}
 
 }
