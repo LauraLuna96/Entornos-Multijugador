13a6b534d11e92fe27f5c2089f23955ec7acad7e
Laura Suonperä Lozano <43136220+LauraLuna96@users.noreply.github.com>
2019-05-26 20:51:24 +0200

    Collision factor de power up (?)
    no entiendo muy bien como va, lo miro luego

--- practica_2018-19/src/main/java/spacewar/PowerUp.java
+++ practica_2018-19/src/main/java/spacewar/PowerUp.java
@@ -6,6 +6,7 @@ public class PowerUp {
 	private final int posX;
 	private final int posY;
 	private final int id;
+	private static final int POWERUP_COLLISION_FACTOR = 200;
 
 	PowerUp(int posX, int posY, int id) {
 		this.posX = posX;
